e42d021e31fb836967338c57b0e533d8a640816f
TienPD3 <TienPD3@icloud.com>
2021-11-27 22:14:34 +0700

    TienPD3: iAP Fake

---
+++ Scripts/iAPVip/MonkeyJunior.js
@@ -0,0 +1,10 @@
+var obj = JSON.parse($response.body);
+
+obj.data.purchased = {
+  product_list: [],
+  is_active: true,
+  time_expire: 0,
+  pay_inapp: true,
+  is_free: false,
+};
+$done({ body: JSON.stringify(obj) });
