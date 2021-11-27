f1f91c08c28a4d492add9f03a42d8faeff3c0804
TienPD3 <TienPD3@icloud.com>
2021-11-27 22:52:25 +0700

    TienPD3: iAP Fake

--- Scripts/iAPVip/MonkeyJunior.js
+++ Scripts/iAPVip/MonkeyJunior.js
@@ -1,10 +1,19 @@
-var obj = JSON.parse($response.body);
+// var obj = JSON.parse($response.body);
 
-obj.data.purchased = {
-  product_list: [],
-  is_active: true,
-  time_expire: 0,
-  pay_inapp: true,
-  is_free: false,
+// obj.data.purchased = {
+//   product_list: [],
+//   is_active: true,
+//   time_expire: 0,
+//   pay_inapp: true,
+//   is_free: false,
+// };
+
+var obj = {
+  is_valid_device: true,
+  has_valid_subscription: true,
+  expiration_date_ms: 4098231805000,
+  is_table_resettable: true,
+  subscription_product_id: "com.kinemaster.sub.annual",
+  state_code: 0,
 };
 $done({ body: JSON.stringify(obj) });
